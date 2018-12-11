3884d1a1a6bc80cf0030422e4eb0c680ce5676a1
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2018-12-11 14:25:15 -0600

    set up file structure; details of read me

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('cheezborger', '1');
+
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('double cheeze', '1');
+
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('The Katie', '0');
